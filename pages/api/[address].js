5a7846e405fd57fecd2ecff7cfcac6b38f7a2771
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2023-02-17 21:06:44 +0530

    Completed API

--- pages/api/[address].js
+++ pages/api/[address].js
@@ -1,14 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { ethers } from "ethers";
-const fs = require("fs");
-import { Web3Storage } from "web3.storage";
 import axios from "axios";
 export default async function handler(req, res) {
   const SUBGRAPH = process.env.SubGraph_URL;
-  console.log(req.query.address);
-  const apiToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEIyOTMzRDhDM2Y5MzkyRjI2MDNjZDZiQUFBZTczRjJhNjNCRjcxYjEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzU3OTk3MjYxOTEsIm5hbWUiOiJBZCBzZXJ2ZXIifQ.bGUQJrYJIA4ugMI_gPUQxHMLPTFuCs0xgkgFK-kBaXw";
-  let client = new Web3Storage({ token: apiToken });
+  const ContractAddress = process.env.NEXT_PUBLIC_Server_ADDRESS;
+  const ABI = process.env.NEXT_PUBLIC_Server_ABI;
+
   let address = req.query.address;
   let query = `{
   publishers(
@@ -20,7 +17,8 @@ export default async function handler(req, res) {
 }`;
   let resus = await axios.post(SUBGRAPH, { query });
   let AdId = await resus.data.data.publishers[0].Advertisers;
-  console.log(AdId);
+  let index = Math.floor(Math.random() * AdId.length);
+  console.log(AdId[index]);
   query = `{
   ads(
     first: 5
@@ -30,6 +28,49 @@ export default async function handler(req, res) {
   }
 }`;
   let resus2 = await axios.post(SUBGRAPH, { query });
-  console.log(resus2.data.data.ads)
-  res.send(resus2.data.data.ads[0].AdData);
+  const x = resus2.data.data.ads[0].AdData;
+  const y = await axios.get(x);
+  const targetUrl = y.data.targetURL || "https://www.google.com";
+  const handleClick = async () => {
+    try {
+      const provider = await new ethers.providers.JsonRpcProvider(
+        "https://polygon-mumbai.g.alchemy.com/v2/oF0VkR1DtxMYpSbUf4RyDwLyclveHuCw",
+        {
+          chainId: 80001,
+        }
+      );
+      const signer = await new ethers.Wallet(
+        "b44242c0805f9bcb4cea019517d45ec806fb4850a15e3b28cdfc6ac261e1cbc5",
+        provider
+      );
+      const contract = await new ethers.Contract(ContractAddress, ABI, signer);
+      const tx = await contract.functions.transferClickReward(
+        AdId[index],
+        address
+      );
+      const signedTx = await signer.signTransaction(tx);
+      const sentTx = await provider.sendTransaction(signedTx);
+      await sentTx.wait();
+      console.log("Clicked");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+    </head>
+    <body>
+      <h1>Ad</h1>
+      <a href="${targetUrl}">
+      <img src="${
+        y.data.ImgLink
+      }" alt="Ad Image" border="1" onClick="${handleClick()}"></img></a>
+      <img src="xyz/abc/${address}/${
+    AdID[index]
+  }" height="1" width="1" style={display:none} />
+    </body>
+  </html>`;
+  res.send(html);
 }
